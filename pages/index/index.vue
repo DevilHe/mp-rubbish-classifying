a429b5e0448d1a3d2116b82e6c37221d2eda9627
DevilHe <929673054@qq.com>
2021-01-10 13:55:21 +0800

    feat:添加首页轮播图

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,5 +1,12 @@
 <template>
 	<view>
+    <view>
+      <swiper indicator-dots circular autoplay interval=3000 style="height: 200px;margin-bottom: 16px;">
+				<swiper-item v-for="(item,index) in swipers" :key="index">
+					<image :src="item" style="width: 100%;height: 100%;"></image>
+				</swiper-item>
+			</swiper>
+    </view>
 		<view style="width: 100%;padding:10px 20px;">
 			<button @click="refuseClassification">垃圾分类</button>
 		</view>
@@ -13,12 +20,12 @@
 export default {
   data() {
     return {
-      
+      swipers: []
     }
   },
   onLoad() {
-
-  },
+		this.swipers=["/static/carousel-img1.jpg", "/static/carousel-img2.jpeg", "/static/carousel-img3.jpg"];
+	},
   methods: {
     // 垃圾分类查询
     refuseClassification() {
